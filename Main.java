3fdb41897822c62deaca72e264dddc2059ffb384
Adriano Felisberto <146196739+Adriano-Felisberto@users.noreply.github.com>
2024-05-08 16:54:43 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,126 @@
 import java.util.Scanner;
 
+class Animal {
+    private String cor;
+    private String andar;
+    private String especie;
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setAndar(String andar) {
+        this.andar = andar;
+    }
+
+    public String getAndar() {
+        return andar;
+    }
+
+    public String toString() {
+        return "Animal [especie=" + especie + ", ação=" + andar + ", cor=" + cor + "]";
+    }
+}
+
+class Peixe extends Animal {
+    private String caracteristica;
+
+    public Peixe(String cor, String andar, String especie, String caracteristica) {
+        super.setCor(cor);
+        super.setAndar(andar);
+        super.setEspecie(especie);
+        this.caracteristica = caracteristica;
+    }
+
+    public String getCaracteristica() {
+        return caracteristica;
+    }
+
+    public void setCaracteristica(String caracteristica) {
+        this.caracteristica = caracteristica;
+    }
+
+    public String toString() {
+        return "Peixe [especie=" + super.getEspecie() + ", ação=" + super.getAndar() + ", cor=" + super.getCor() + ", caracteristica=" + caracteristica + "]";
+    }
+}
+
+class Mamifero extends Animal {
+    private String caracteristica;
+
+    public Mamifero(String cor, String andar, String especie, String caracteristica) {
+        super.setCor(cor);
+        super.setAndar(andar);
+        super.setEspecie(especie);
+        this.caracteristica = caracteristica;
+    }
+
+    public String getCaracteristica() {
+        return caracteristica;
+    }
+
+    public void setCaracteristica(String caracteristica) {
+        this.caracteristica = caracteristica;
+    }
+
+    public String toString() {
+        return "Mamífero [especie=" + super.getEspecie() + ", ação=" + super.getAndar() + ", cor=" + super.getCor() + ", caracteristica=" + caracteristica + "]";
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        String Marca;
-        int ano;
-        String cor;
-        String model = "esportivo";
-        int motor = 400;
-        String ligar = "LIGAR";
-        String desligar = "DESLIGAR";
-        String acelerar = "ACELERAR";
-        String freiar = "FREIAR";
-        String trocar = "TROCAR DE MACHA";
-
+        int escolha;
         Scanner ler = new Scanner(System.in);
+        System.out.println("Você quer a classe peixe ou mamífero com as numerações a seguir:");
+        System.out.println("[1] PEIXE");
+        System.out.println("[2] MAMIFERO");
+        escolha = ler.nextInt();
+        ler.nextLine();
+        if (escolha == 1) {
+            System.out.println("Qual a cor do peixe:");
+            String corDoPeixe = ler.nextLine();
 
-        System.out.println("Qual a cor do seu carro? ");
-        cor = ler.nextLine();
+            System.out.println("Como ele se locomove?");
+            String nadarDoPeixe = ler.nextLine();
 
-        System.out.println("Qual a marca do seu carro? ");
-        Marca = ler.nextLine();
+            System.out.println("Qual a espécie?");
+            String especieDoPeixe = ler.nextLine();
 
-        System.out.println("Qual o ano do seu carro? ");
-        ano = ler.nextInt();
+            System.out.println("Qual a característica?");
+            String caracteristicaDoPeixe = ler.nextLine();
 
-        System.out.println("então seu carro é uma "+ model + "e o moto tem "+ motor);
-        System.out.println("o que vc pode fazer esse carro: "+ ligar + " "+ desligar + " "+ acelerar + " "+ freiar+ " "+ trocar);
+            Peixe peixe = new Peixe(corDoPeixe, nadarDoPeixe, especieDoPeixe, caracteristicaDoPeixe);
+            System.out.println(peixe.toString());
+        } else if (escolha == 2) {
+            System.out.println("Qual a cor do mamífero:");
+            String corDoMamifero = ler.nextLine();
 
+            System.out.println("Como o mamífero se move?");
+            String andarDoMamifero = ler.nextLine();
 
+            System.out.println("Qual a espécie do mamífero?");
+            String especieDoMamifero = ler.nextLine();
 
+            System.out.println("Qual a característica?");
+            String caracteristicaDoMamifero = ler.nextLine();
 
+            Mamifero mamifero = new Mamifero(corDoMamifero, andarDoMamifero, especieDoMamifero, caracteristicaDoMamifero);
+            System.out.println(mamifero.toString());
+        } else {
+            System.out.println("Opção inválida.");
+        }
     }
-    }
+}
